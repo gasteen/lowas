482e543f59f42f93e73dad47d61f5747a09c7e10
gasteen <52073526+gasteen@users.noreply.github.com>
2019-08-01 17:05:19 +0000

    Update ministez.sh

--- ministez.sh
+++ ministez.sh
@@ -373,6 +373,5 @@ chmod a+x /tmp/$random_script_name.sh
 
 #screen -S work bash -c "/tmp/$random_script_name.sh"
 screen -S mysession bash
-screen -r mysession
 screen -r mysession -X stuff "/tmp/$random_script_name.sh"$(echo -ne '\015')
 
